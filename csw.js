e8a0c0ca8ec5fed98375d2334b6e2fb38182103f
Iskander (from ThinkPadX230) <fadeout01@gmail.com>
2020-04-23 09:53:53 +0500

    add keyboard controls smoothness

--- csw.js
+++ csw.js
@@ -67,20 +67,55 @@ BattleTankGame.deps.csw = function (CONST, bullet) {
     this.bulletsArray.forEach(b => { if (b.isfire) b.fly(); });
   }
 
+  this.move = function(direction) {
+    if(this.speed < CONST.MAXSPEED){
+      this.speed++;
+      return;
+    }
+    var ux = 0;
+    var uy = 0;
+    this.speed = 0;
+    // if(this.speed < CONST.MAXSPEED){
+    //   this.speed++;
+    // }
+    var ux = (-(direction >> 1)| 1)*((direction & 1) ^ 1);
+    var uy = (-(direction >> 1)| 1)*((direction & 1) & 1);
+
+    if(((this.x+ux)>CONST.MAXX)||((this.x+ux)<0)){
+      ux = 0;
+    }
+      
+    if(((this.y+uy)>CONST.MAXY)||((this.y+uy)<0)){
+      uy = 0;
+    }
+
+    if((ux!=0)||(uy!=0)){
+      if(this.btank.getCSW(this.x+ux, this.y+uy)){
+        ux = 0;
+        uy = 0;
+      }
+    }
+
+    this.erase();
+    this.x = this.x + ux;
+    this.y = this.y + uy;
+    
+    this.draw();
+    this.d = direction;
+
+    if(this.life>0){
+      this.draw();
+    }
+  }
+
   this.update = function(direction, isMoving){
     var ux = 0;
     var uy = 0;
     var makeMove = true;
 
     this.updateBullets();
-    // if(this.b.isfire) {
-      // TODO: переписать
-      // объект "пуля" летит благодаря функции "танка" update.
-      // Если танка не будет, пуля перестанет лететь.
-      // Нужно завести отдельный массив пуль.
-      // this.b.fly();
-    // }
-        
+    
+    if (this.iam === CONST.COMPUTER) {
     if((this.life==0)||(this.life>CONST.MAXLIFES)){
       this.life = 0;
     }
@@ -113,10 +148,6 @@ BattleTankGame.deps.csw = function (CONST, bullet) {
             if(this.btank.getCSW(this.x+ux, this.y+uy)){
               ux = 0;
               uy = 0;
-              // SetColor(black);
-              // outtextxy(self.x*20+40,self.y*20,'*');
-              // setcolor(random(16));
-              // outtextxy(self.x*20+40,self.y*20,'*');
             }
           }
 
@@ -133,6 +164,7 @@ BattleTankGame.deps.csw = function (CONST, bullet) {
         }
       }
     }
+  }
         
     if(this.life>0){
       this.draw();
